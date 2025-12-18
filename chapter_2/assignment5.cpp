eefd9f2ec2076b6f0d78bb9caf2e71b0499349c6
Kaushik <aaditya.kaushik@hp.com>
2025-12-18 10:45:02 +0530

    cahpter 2

---
+++ chapter_2/assignment5.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class EmployeeDetails
+{
+int id;
+std::string name;
+std::string department;
+bool working;
+
+public:
+saveEmployeeTODatabase()
+printEmployeeDetailReportXML()
+printEmployeeDetailReportCSV()
+terminateEmployee()
+bool
+isWorking()
+}; 
