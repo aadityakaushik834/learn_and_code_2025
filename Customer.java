10368f164a98074ac0f4121934bc2044add49bea
Kaushik <aaditya.kaushik@hp.com>
2026-03-31 16:46:42 +0530

    resolved comment on PR

--- Customer.java
+++ Customer.java
@@ -2,10 +2,13 @@ public class Customer {
     private Wallet wallet;
 
     public Customer(Wallet wallet) {
+        if (wallet == null) {
+            throw new IllegalArgumentException("Wallet cannot be null");
+        }
         this.wallet = wallet;
     }
 
     public boolean pay(double amount) {
-        return wallet != null && wallet.withdraw(amount);
+        return wallet.withdraw(amount);
     }
 }
